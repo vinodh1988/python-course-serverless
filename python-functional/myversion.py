ce3e85f4a9a07c1b7567dff42199c404cdce497a
vinodhkc@vinodhconnects.onmicrosoft.com <vinodhkc@vinodhconnects.onmicrosoft.com>
2021-12-24 16:34:15 +0530

    lambda-commit

--- python-functional/myversion.py
+++ python-functional/myversion.py
@@ -8,3 +8,7 @@ def stringProcess(x):
      return "{} -> {}".format(x,len(x))
 
 print(mymap(stringProcess,["Raj","Ravi","India","International","World"]))
+
+print(mymap(lambda a:a*a,[2,4,56,5,46,45,6]))
+
+print(mymap(lambda a: "EVEN" if a%2==0 else "ODD",[34,56,241,234,111,215,124,153]) )
